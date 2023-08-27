25530ee88d63f76ea9402425ebaf2d689aecc4a3
julianuri <julian.uribe.contact@gmail.com>
2023-08-27 11:24:20 -0400

    Removed board

--- src/types/consts/BoardsEnum.tsx
+++ src/types/consts/BoardsEnum.tsx
@@ -1,7 +1,6 @@
 const BOARDS = [
-  { id: 0, image: '/static/boards/colorful-snake.png' },
-  { id: 1, image: '/static/boards/snakes_and_ladders_1.png' },
-  { id: 2, image: '/static/boards/snakes_and_ladders_2.png' },
+  { id: 0, image: '/static/boards/snakes_and_ladders_1.png' },
+  { id: 1, image: '/static/boards/snakes_and_ladders_2.png' },
 ];
 
 export default BOARDS;
